edc4fafa34a637ef13d1894e3f6ca9773ba4d7d6
jvwijhe <jensvanwijhe@live.nl>
2022-06-22 14:00:07 +0200

    task list item, project observer

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,58 @@
 
 <template>
+<div class="relative h-screen w-screen bg-gray-50 overflow-y-auto">
 <authenticator>
-  <section class="my-12">
-    <div class="container mx-auto">
+  <section class="py-12">
+    <div class="container mx-auto max-w-6xl">
     <h1 class="text-3xl font-bold underline">
       Aws Amplify Task Manager
     </h1>
+
     </div>
   </section>
 
   <section class="my-12">
-    <div class="container mx-auto bg-gray-100 p-6 rounded flex gap-6">
-      <input type="text" class="p-3 rounded" v-model="name" placeholder="Todo name">
-      <input type="text" class="p-3 rounded" v-model="description" placeholder="Todo description">
+
+    <div class="container mx-auto max-w-6xl  p-6 rounded border border-gray-200">
+      <p class="text-gray-500 mb-3">Create a project to get started</p>
+      <div class="flex gap-6">
+        <input type="text" class="p-3 rounded" v-model="name" placeholder="Project name">
+        <input type="text" class="p-3 rounded" v-model="description" placeholder="Project description">
       <!-- <select v-model="projectID">
         <option v-for="project in projects" :value="project.id" :key="project.id">{{project.name}}</option>
       </select> -->
 
-    <div class="flex gap-3">
-      <button v-on:click="storeProject" class="bg-blue-500 p-3 rounded text-white">Create Project</button>
-    </div>
+        <div class="flex gap-3">
+          <button v-on:click="storeProject" class="bg-blue-500 p-3 rounded text-white flex gap-1 items-center">
+            <span>
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
+              <path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd" />
+            </svg>
+            </span>
+            <span class="text-sm">Create Project</span></button>
+        </div>
+      </div>
     </div>
   </section>
 
     <section class="my-12">
-        <div class="container mx-auto">
-    <ul class="grid grid-cols-1 gap-6">
-      <li v-for="project in projects" :key="project.id">
-        <ProjectListItem :project="project"/> 
-        </li>
-      </ul>
+        <div class="container mx-auto max-w-6xl">
+          <h3 class="font-semibold text-lg text-gray-700">Your projects:</h3>
+          <template v-if="projects.length">
+          <ul class="grid grid-cols-1 gap-6 mt-3">
+            <li v-for="project in projects" :key="project.id">
+              <ProjectListItem :project="project"/> 
+              </li>
+            </ul>
+          </template>
+          <template v-else>
+            <p class="text-gray-700">No projects created yet.</p>
+          </template>
         </div>
     </section>
 
 </authenticator>
+</div>
 </template>
 
 <script >
@@ -87,6 +106,9 @@ export default {
             "description": description.value,
           })
         );
+
+        name.value = ''
+        description.value = ''
       } catch (error) {
          console.log(error)
       }
--- src/components/Project/List/Item/ProjectListItem.vue
+++ src/components/Project/List/Item/ProjectListItem.vue
@@ -1,41 +1,31 @@
 <template>
-    <div class="p-6 bg-gray-50 rounded">
-        <span class="block text-xs text-gray-500">#{{project.id}}</span>
+    <div class="relative p-6 bg-white  shadow rounded">
+        <!-- <span class="block text-xs text-gray-500">#{{project.id}}</span> -->
         <a href="/" class="my-1 block text-xl font-semibold text-gray-800">{{project.name}}</a>
         
         <p class="text-gray-500">{{project.description}}</p>
 
         <div class="w-full">
             <div class="flex gap-3 my-3">
-             <input type="text" class="p-3 rounded" v-model="name" placeholder="Todo name">
-            <input type="text" class="p-3 rounded" v-model="description" placeholder="Todo description">
+             <input type="text" class="p-3 rounded bg-gray-50" v-model="name" placeholder="Task name">
+            <input type="text" class="p-3 rounded bg-gray-50" v-model="description" placeholder="Task description">
             <button v-on:click="storeTask" class="bg-blue-500 p-3 rounded text-white text-sm">Create Task</button>
             </div>
 
 
             <div class="mt-6" v-if="tasks.length">
                 <span class="font-bold text-sm text-gray-700">Tasks:</span>
-                <ul>
-                    <li v-for="task in tasks" :key="task.id">
-                        <button type="button" @click="updateTaskStatus(task)" class="flex items-center">
-                            <template v-if="task.completed">
-                                <span class="text-green-500 inline-block">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
-                            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
-                            </svg>
-                                </span>
-                            </template>
-                            <template v-else>
-                                <span></span>
-                            </template>
-                            <span class="ml-2 text-gray-700 text-sm">{{task.name}}</span>
-                        </button>
-                    </li>
-                </ul>
+                <div class="mt-3 grid grid-cols-1 gap-3">
+                    <TaskListItem :task="task" v-for="task in tasks" :key="task.id" />
+                </div>
             </div>
         </div>
-        <div class="mt-3">
-            <button type="button" @click="deleteProject" class="text-red-500 font-semibold">Delete</button>
+        <div class="absolute right-0 top-0 m-3">
+            <button type="button" @click="deleteProject" class="bg-red-100 text-red-500 p-2 hover:bg-red-200 rounded font-semibold">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 inline-block" viewBox="0 0 20 20" fill="currentColor">
+                <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
+                </svg>
+            </button>
         </div>
     </div>
 </template>
@@ -46,9 +36,15 @@
     import { DataStore } from '@aws-amplify/datastore';
     import { Project, Task } from '../../../../models';
 
+    import TaskListItem from '@/components/Task/List/Item/TaskListItem.vue'
+
+
 export default {
     props: {
         project: Object
+    },
+    components: {
+        TaskListItem
     },
   setup(props) {
         const tasks = ref([]);
@@ -57,12 +53,19 @@ export default {
 
         onMounted(async () => {
             await getTasks();
-            // tasks.value = (await DataStore.query(Task)).filter(t => t.projectID ===  props.project.id);
         });
 
 
         const getTasks = async () => {
-             tasks.value = (await DataStore.query(Task)).filter(t => t.projectID ===  props.project.id);
+            console.log(props.project.id)
+            DataStore.observeQuery(
+                Task,
+                t => t.projectID("eq",props.project.id),
+                ).subscribe(snapshot => {
+                const { items } = snapshot;
+                console.log(items)
+                  tasks.value = items
+                });
         }
 
         const deleteProject = async () => {
@@ -77,7 +80,9 @@ export default {
                         updated.completed =  !originalTask.completed
                 })
             );
-             await getTasks();
+
+             
+            //  await getTasks();
       }
 
         const storeTask = async() => {
@@ -90,7 +95,9 @@ export default {
                     "completed": false
                 })
                 );
-                await getTasks()
+                name.value = ''
+                description.value = ''
+                // await getTasks()
             } catch (error) {
                 console.log(error)
             }
